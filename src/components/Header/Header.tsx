3d4a7ac113062f10a208d9527be9742f620ca60a
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2024-10-30 08:40:01 +0700

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
     <>
       {/* Header */}
       <header className="flex items-center justify-between py-6    md:rounded-full rounded-none  px-4 ">
-        <h1 className="text-4xl font-dancing  text-primary hidden md:block">
+        <h1 className="text-4xl font-bold font-poppins text-primary hidden md:block">
           Rifki 
         </h1>
 
