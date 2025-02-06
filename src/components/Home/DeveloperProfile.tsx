a0177bd920bff20391006a638bee0eb9a3d56c4c
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2025-02-06 17:44:37 +0700

    update

--- src/components/Home/DeveloperProfile.tsx
+++ src/components/Home/DeveloperProfile.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { useState, useEffect } from "react";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { cn } from "@/lib/utils";
